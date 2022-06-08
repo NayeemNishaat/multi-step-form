365ad91161193aa0942237cd819f571fd85c1a7c
NayeemNishaat <rxNayeemIMI@gmail.com>
2022-06-08 18:34:53 +0600

    added

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <header className="sticky">
+        <header className="sticky w-[1440px] overflow-scroll">
             <h1 className="bg-green-400 py-5 text-center">
                 Train Ticket Reservation System
             </h1>
